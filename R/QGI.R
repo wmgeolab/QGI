c5e210daed4aaa9ef4391a2ba5f9de7feb5ffe40
DanRunfola <dan@danrunfola.com>
2020-10-12 13:50:06 -0400

    Renaming vars

--- R/QGI.R
+++ R/QGI.R
@@ -6,71 +6,81 @@ library(roxygen2)
 #' Plot Propensity Model
 #'
 #' Matches samples of groups of treated and control observations that have a spatial distribution (i.e., latitude and longitude coordinates), so that matched groups have (1) similar covariate distributions [via a propensity score matching], and (2) do not result in paired matches being geographically proximate.  The package also provides a distance-decay estimate of the treatment effect (i.e., the impact a treatment had on a defined outcome) following the procedure outlined in Runfola and Batra et al. (2020) <https://doi.org/10.3390/su12083225>.
-#' @param rDF Takes a dataset with a 'distance' column...
-#' @param outcome_var Outcome variable (i.e., under 5 child mortality)
+#' @param df A R dataframe containing your observations
+#' @param distanceCol The column in your dataframe containing the distance in kilometers between each observation and the intervention locations
 #' @param controlVars List of all independent variables to be used to model propensity and outcome
-#' @param sample_density Total number of runs
-#' @param lower_dist_bound Smallest distance (in decimal degrees) to calculate results for. Defaults to 0.01.
-#' @param upper_dist_bound Largest distance (in decimal degrees) to calculate results for. Defaults to 0.5.
-#' @param maximum_match_diff From 0 to 1, the largest difference in match quality allowed. Defaults to 0.9.
+#' @param outcomeVar The column in your dataframe containing the outcome variable 
+#' @param density Total number of estimations to be made, equally spaced between the lower and upper distance bound. Defaults to 50.
+#' @param lowerDistBound Smallest distance (in kilometers) to calculate results for. Defaults to 0km.
+#' @param upperDistBound Largest distance (in kilometers) to calculate results for. Defaults to the median of distance in the data.
+#' @param enforcedMinimumDistance Minimum distance (in kilometers) between a location considered treated and untreated, across the full dataset.  Defaults to 5km.
+#' @param maximum_match_diff From 0 to 1, the largest difference in match quality allowed. 
+#' @param cores The number of computer cores to use to fit the QGI model.  Defaults to number of cores on machine - 1.
 #' @keywords
 #' @export
 #' @examples
-#' plotPropensityModel(read.csv("../rDF.csv"), independent_vars, x,x,x, 30*16)
+#' QGI(read.csv("../df.csv"), independent_vars, x,x,x, 30*16)
 
-#Allow user to change the name of the distance column.  Right now it is hard coded as "Distance".
-
-QGI <- function(rDF, 
-                                controlVars, 
-                                outcome_var, 
-                                outcome_label, 
-                                sample_density, 
-                                lower_dist_bound=0.01, 
-                                upper_dist_bound=0.5, 
-                                maximum_match_diff=0.9, 
-                                untreated_thresh=0.3,
-                                task = 'gef') {
+QGI <- function(df, 
+                distanceCol = "distance"
+                controlVars, 
+                outcomeVar = "outcome", 
+                density = 50, 
+                lowerDistBound=0.0, 
+                upperDistBound="Default", 
+                maximum_match_diff=0.9, 
+                enforcedMinimumDistance=5,
+                cores = "Default") {
   
-  cores=detectCores()
+  if(cores == "Default")
+  {
+    cores=detectCores()
+  }
+
   cl <- makeForkCluster(cores[1]-1) 
   registerDoParallel(cl)
+
+  if(upperDistBound == "Default")
+  {
+    upperDistBound = as.numeric(summary(df[,distanceCol])[3])
+  }
   
   mc = 0
   #define sampling frame, with as much coverage as possible
   #given a defined density
   
-  if(sample_density <= 1){
-    sample_density = 2
+  if(density <= 1){
+    density = 2
     print("Sample Density set to minimum allowed (2)")
   }
 
-  #trials = seq(upper_dist_bound, lower_dist_bound, (lower_dist_bound-upper_dist_bound)/(sample_density-1))
-  trials = seq(lower_dist_bound, upper_dist_bound, (upper_dist_bound-lower_dist_bound)/(sample_density-1))
+  #trials = seq(upperDistBound, lowerDistBound, (lowerDistBound-upperDistBound)/(density-1))
+  trials = seq(lowerDistBound, upperDistBound, (upperDistBound-lowerDistBound)/(density-1))
   
   mc <- foreach(i = 1:length(trials), .combine=rbind) %dopar% {
     set.seed(i)
     
-    rDF$distanceTrue <- rDF$distance
+    df$distanceTrue <- df$distance
     
-    rDF$Treatment <- 999
+    df$Treatment <- 999
     
     #Change the distance for the threshold.
     #We actually need to do this a few times, to construct the distance bands.
     dist_thresh <- trials[i] 
     if (task == 'fao'){
-      #rDF$Treatment[rDF$distanceTrue > 0.1 & rDF$distanceTrue < dist_thresh] <- 0
-      #rDF$Treatment[rDF$distanceTrue > dist_thresh] <- 0
-      #rDF$Treatment[rDF$distanceTrue == 0] <- 1
-      rDF$Treatment[rDF$distanceTrue <= 0 + dist_thresh] <- 1
-      rDF$Treatment[rDF$distanceTrue >= untreated_thresh + dist_thresh] <- 0
+      #df$Treatment[df$distanceTrue > 0.1 & df$distanceTrue < dist_thresh] <- 0
+      #df$Treatment[df$distanceTrue > dist_thresh] <- 0
+      #df$Treatment[df$distanceTrue == 0] <- 1
+      df$Treatment[df$distanceTrue <= 0 + dist_thresh] <- 1
+      df$Treatment[df$distanceTrue >= enforcedMinimumDistance + dist_thresh] <- 0
     }else {
-      rDF$Treatment[rDF$distanceTrue <= dist_thresh] <- 1
-      rDF$Treatment[rDF$distanceTrue > (dist_thresh + upper_dist_bound)] <- 0
+      df$Treatment[df$distanceTrue <= dist_thresh] <- 1
+      df$Treatment[df$distanceTrue > (dist_thresh + upperDistBound)] <- 0
     }
       
-    rDF <- rDF[!rDF$Treatment == 999,]
+    df <- df[!df$Treatment == 999,]
     
-    rDF[, !(names(rDF) == "distance")]
+    df[, !(names(df) == "distance")]
     
     #Choose pscore variables
     pVars <- c(controlVars,"Treatment")
@@ -87,32 +97,32 @@ QGI <- function(rDF,
       f1 <- as.formula(paste("Treatment", paste(controlVars, collapse = " + "), sep = " ~ "))
     }
     
-    pscore.Calc <- matchit(f1, data=na.omit(rDF[pVars], cols=pVars), method="nearest", distance="logit")
+    pscore.Calc <- matchit(f1, data=na.omit(df[pVars], cols=pVars), method="nearest", distance="logit")
     
-    matched.dta <- match.data(pscore.Calc)
+    matched.df <- match.data(pscore.Calc)
     
     
     #construct outcome modeling formula
     #the last 3 debug lines are required for our data, the else case is the simpler generalized version
     if (task == 'uganda') {
       #we can assume user already has a reasonable $outcome measurement.
-      matched.dta$outcome <- (matched.dta$toJoin_LSMS_2011_geogAsset_h14q5.y - matched.dta$toJoin_LSMS_2009_geogAsset_h14q5.y) * 0.00027
+      matched.df$outcome <- (matched.df$toJoin_LSMS_2011_geogAsset_h14q5.y - matched.df$toJoin_LSMS_2009_geogAsset_h14q5.y) * 0.00027
       
       exclude_some_var2 <- c("Treatment", controlVars)
       exclude_some_var2 <- exclude_some_var2[!exclude_some_var2 %in% c("toJoin_LSMS_2011_geogAsset_h14q5.y","extracted_data_gdp_grid.na.mean.y")]
       f2 <- as.formula(paste("outcome", paste(exclude_some_var2, collapse = " + "), sep = " ~ "))
     } else {
-      idx = as.numeric(rownames(matched.dta))
-      matched.dta$outcome <- rDF[idx,][[outcome_var]]
+      idx = as.numeric(rownames(matched.df))
+      matched.df$outcome <- df[idx,][[outcomeVar]]
       f2 <- as.formula(paste("outcome", paste(c("Treatment", controlVars), collapse = " + "), sep = " ~ "))
     }
     
-    trtModel <- lm(f2, data= na.omit(matched.dta[aVars], cols=pVars))
+    trtModel <- lm(f2, data= na.omit(matched.df[aVars], cols=pVars))
     
     
     match_diff = abs(summary(pscore.Calc)$sum.matched[[1]][1] - summary(pscore.Calc)$sum.matched[[2]][1])
     
-    return( list(i, dist_thresh, trtModel$coef["Treatment"][[1]], nrow(rDF), match_diff, summary(trtModel)$r.squared, coef(summary(trtModel))[2,4], coef(summary(trtModel))[2,2], nrow(matched.dta)))
+    return( list(i, dist_thresh, trtModel$coef["Treatment"][[1]], nrow(df), match_diff, summary(trtModel)$r.squared, coef(summary(trtModel))[2,4], coef(summary(trtModel))[2,2], nrow(matched.df)))
     
   }
 
